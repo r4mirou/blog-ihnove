65420f45ff7ca716da082af0c0e7915b4e9e5105
Ramiro Lima <ramirolima.dev@gmail.com>
2020-09-11 23:07:16 -0300

    first production test

--- public/sw.js
+++ public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d7e2720bd8ffaf7c7309.js"
+    "url": "webpack-runtime-121fae912953d37b8fa4.js"
   },
   {
     "url": "framework-a38040c4749cf445737f.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "86416b00d5ab6e8ee8ddb30401e429ec"
+    "revision": "72ccbecdb730d807a8bff24e403bcd65"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c9875e2473a7c518755f.js"
